b562b2bce699445af2e03246532c3e7306123f91
rpgaudioso <rpgaudioso@gmail.com>
2019-03-24 16:55:28 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { AuthServiceConfig, GoogleLoginProvider, SocialLoginModule } from "angular-6-social-login";
+import { AuthServiceConfig, GoogleLoginProvider, SocialLoginModule } from 'angular-6-social-login';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { LoginHelperComponent } from './login-helper/login-helper.component';
